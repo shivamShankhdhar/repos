d85a49c659db144c1e5c0dbde1e5ad94bbf81d25
shivam shankhdhar <39639414+shivamShankhdhar@users.noreply.github.com>
2023-06-20 11:45:20 +0530

    second commit

---
+++ LAB-3/LAB-3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LAB_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 48; i <=57; i++) {
+                Console.WriteLine("ASCII Value= {0} ASCII character {1}",i,(char)i);
+            }
+            
+
+        }
+    }
+}
+
+//Q9: Display ascii characters from 48 to 57.
+
